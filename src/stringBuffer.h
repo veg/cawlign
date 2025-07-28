1e2f33e1eceea8112d24eb73920ee679ac27111c
Sergei <spond@temple.edu>
2025-07-28 13:09:50 -0700

    Adding function comments

--- src/stringBuffer.h
+++ src/stringBuffer.h
@@ -1,39 +1,107 @@
-#ifndef __STRINGBUFFER__
+'''#ifndef __STRINGBUFFER__
 #define __STRINGBUFFER__
 
 #include "alignment.h"
 
 //__________________________________________________________________________________________
 
+/**
+ * @brief A simple string buffer class that allows for dynamic resizing.
+ * 
+ */
 class StringBuffer {
 
   char *sData;
   unsigned long sLength, saLength;
 
 public:
+  /**
+   * @brief Construct a new String Buffer object
+   * 
+   */
   StringBuffer(void);
+  /**
+   * @brief Destroy the String Buffer object
+   * 
+   */
   ~StringBuffer(void);
 
+  /**
+   * @brief Get the underlying C string.
+   * 
+   * @return char* A pointer to the underlying C string.
+   */
   char *getString(void) const { return sData; }
+  /**
+   * @brief Append a character to the string buffer.
+   * 
+   * @param c The character to append.
+   */
   void appendChar(const char);
+  /**
+   * @brief Append a C string to the string buffer.
+   * 
+   * @param buffer The C string to append.
+   * @param length The length of the C string to append. If -1, the entire string is appended.
+   */
   void appendBuffer(const char *, const long = -1);
+  /**
+   * @brief Reset the string buffer to an empty state.
+   * 
+   */
   void resetString(void);
+  /**
+   * @brief Swap the contents of this string buffer with another.
+   * 
+   * @param other The other string buffer to swap with.
+   */
   void swap(StringBuffer &);
+  /**
+   * @brief Get the length of the string buffer.
+   * 
+   * @return unsigned long The length of the string buffer.
+   */
   unsigned long length(void) const { return sLength; }
+  /**
+   * @brief Reset the length of the string buffer.
+   * 
+   * @param newL The new length of the string buffer.
+   */
   void reset_length(unsigned long newL) {
     if (newL < sLength) {
       sLength = newL;
     }
   }
 
+  /**
+   * @brief Set a character at a specific index.
+   * 
+   * @param i The index to set the character at.
+   * @param c The character to set.
+   * @return char The character that was previously at the index.
+   */
   char setChar(const long i, const char c) {
     char oc = sData[i];
     sData[i] = c;
     return oc;
   }
     
+  /**
+   * @brief Get a character at a specific index.
+   * 
+   * @param i The index to get the character from.
+   * @return char The character at the index.
+   */
   char getChar(const long i) const { return sData[i]; }
+  /**
+   * @brief Flip the string buffer in place.
+   * 
+   */
   void flip (void);
+  /**
+   * @brief Detach the underlying C string from the string buffer.
+   * 
+   */
   void detach (void) { sData = nullptr;}
 
   static long sbDefaultLength, sbDefaultBoost;
@@ -41,6 +109,10 @@ public:
 
 //__________________________________________________________________________________________
 
+/**
+ * @brief A simple vector class for floating point numbers that allows for dynamic resizing.
+ * 
+ */
 class VectorFP {
 
   cawlign_fp *vData;
@@ -48,15 +120,61 @@ class VectorFP {
   unsigned long vLength, vaLength;
 
 public:
+  /**
+   * @brief Construct a new VectorFP object
+   * 
+   */
   VectorFP(void);
+  /**
+   * @brief Destroy the VectorFP object
+   * 
+   */
   ~VectorFP(void);
 
+  /**
+   * @brief Append a value to the vector.
+   * 
+   * @param value The value to append.
+   */
   void appendValue(const cawlign_fp);
+  /**
+   * @brief Append multiple values to the vector.
+   * 
+   * @param values The values to append.
+   * @param count The number of values to append.
+   */
   void appendValues(const cawlign_fp*, long);
+  /**
+   * @brief Store a value at a specific index.
+   * 
+   * @param value The value to store.
+   * @param index The index to store the value at.
+   */
   void storeValue(const cawlign_fp, const unsigned long);
+  /**
+   * @brief Get a value at a specific index.
+   * 
+   * @param idx The index to get the value from.
+   * @return cawlign_fp The value at the index.
+   */
   cawlign_fp value(const long idx) { return vData[idx]; }
+  /**
+   * @brief Get the length of the vector.
+   * 
+   * @return unsigned long The length of the vector.
+   */
   unsigned long length(void) const { return vLength; }
+  /**
+   * @brief Get the underlying C array.
+   * 
+   * @return const cawlign_fp* A pointer to the underlying C array.
+   */
   const cawlign_fp * values (void) {return vData;}
+  /**
+   * @brief Get the underlying C array.
+   * 
+   * @return cawlign_fp* A pointer to the underlying C array.
+   */
   cawlign_fp * rvalues (void) {return vData;}
 
   static long vDefaultLength, vDefaultBoost;
@@ -64,6 +182,10 @@ public:
 
 //__________________________________________________________________________________________
 
+/**
+ * @brief A simple vector class for long integers that allows for dynamic resizing.
+ * 
+ */
 class Vector {
 
   long *vData;
@@ -71,22 +193,87 @@ class Vector {
   unsigned long vLength, vaLength;
 
 public:
+  /**
+   * @brief Construct a new Vector object
+   * 
+   */
   Vector(void);
+  /**
+   * @brief Destroy the Vector object
+   * 
+   */
   ~Vector(void);
 
+  /**
+   * @brief Append a value to the vector.
+   * 
+   * @param value The value to append.
+   */
   void appendValue(const long);
+  /**
+   * @brief Append another vector to this vector.
+   * 
+   * @param other The other vector to append.
+   */
   void appendVector(const Vector &);
+  /**
+   * @brief Extract the minimum value from a VectorFP object.
+   * 
+   * @param other The VectorFP object to extract the minimum value from.
+   * @return long The minimum value.
+   */
   long extractMin(VectorFP &);
+  /**
+   * @brief Reset the vector to an empty state.
+   * 
+   */
   void resetVector(void);
+  /**
+   * @brief Remove a value at a specific index.
+   * 
+   * @param index The index to remove the value at.
+   */
   void remove(const unsigned long);
+  /**
+   * @brief Store a value at a specific index.
+   * 
+   * @param value The value to store.
+   * @param index The index to store the value at.
+   */
   void storeValue(const long, const unsigned long);
+  /**
+   * @brief Store another vector at a specific index.
+   * 
+   * @param other The other vector to store.
+   * @param index The index to store the vector at.
+   */
   void storeVector(const Vector &, const unsigned long);
+  /**
+   * @brief Sort the vector in place.
+   * 
+   */
   void sort(void);
+  /**
+   * @brief Swap the contents of this vector with another.
+   * 
+   * @param other The other vector to swap with.
+   */
   void swap(Vector &);
+  /**
+   * @brief Get a value at a specific index.
+   * 
+   * @param idx The index to get the value from.
+   * @return long The value at the index.
+   */
   long value(const long idx) const;
+  /**
+   * @brief Get the length of the vector.
+   * 
+   * @return unsigned long The length of the vector.
+   */
   unsigned long length(void) const { return vLength; }
 
   static long vDefaultLength, vDefaultBoost;
 };
 
-#endif
+#endif''
--- src/tn93_shared.h
+++ src/tn93_shared.h
@@ -1,4 +1,4 @@
-#ifndef 	__TN93SHARED__
+'''#ifndef 	__TN93SHARED__
 #define 	__TN93SHARED__
 
 #include <iostream>
@@ -32,6 +32,10 @@ using namespace std;
 #define MIN(a,b) (a) < (b) ? (a) : (b)
 #define MAX(a,b) (a) > (b) ? (a) : (b)
 
+/**
+ * @brief A struct to hold the first and last non-gap characters in a sequence.
+ * 
+ */
 struct sequence_gap_structure {
   
   long first_nongap,
@@ -39,6 +43,10 @@ struct sequence_gap_structure {
        resolved_start,
        resolved_end;
   
+  /**
+   * @brief Construct a new sequence gap structure object
+   * 
+   */
   sequence_gap_structure (void) {
     first_nongap    = LONG_MAX;
     last_nongap     = 0L;
@@ -48,10 +56,50 @@ struct sequence_gap_structure {
   
 };
 
+/**
+ * @brief Initialize the random number generator.
+ * 
+ * @param s The seed for the random number generator.
+ */
 void init_genrand(unsigned long s);
+/**
+ * @brief Generate a random 32-bit integer.
+ * 
+ * @return unsigned long A random 32-bit integer.
+ */
 unsigned long genrand_int32(void);
+/**
+ * @brief Compute the TN93 distance between two sequences.
+ * 
+ * @param s1 The first sequence.
+ * @param s2 The second sequence.
+ * @param L The length of the sequences.
+ * @param matchMode The mode to use for matching ambiguous characters.
+ * @param randomize A pointer to an array of random numbers.
+ * @param min_overlap The minimum overlap between the two sequences.
+ * @param resolvedCounts A pointer to an array of resolved counts.
+ * @param zero_diag A flag to indicate whether to zero the diagonal.
+ * @param cnt The number of sequences.
+ * @param count1 The number of sequences in the first group.
+ * @param count2 The number of sequences in the second group.
+ * @param gaps1 A pointer to a sequence gap structure for the first sequence.
+ * @param gaps2 A pointer to a sequence gap structure for the second sequence.
+ * @return double The TN93 distance between the two sequences.
+ */
 double		computeTN93 (const char * s1, const char *s2,  const unsigned long L, const char matchMode, const long * randomize, const long min_overlap, unsigned long* = NULL, const double = 0.0, const unsigned long cnt = 0, const long count1 = 1, const long count2 = 1, const sequence_gap_structure * = NULL, const sequence_gap_structure * = NULL);
 
+/**
+ * @brief Compute the differences between two sequences.
+ * 
+ * @param s1 The first sequence.
+ * @param s2 The second sequence.
+ * @param L The length of the sequences.
+ * @param matchMode The mode to use for matching ambiguous characters.
+ * @param storage A vector to store the differences in.
+ * @param gaps1 A pointer to a sequence gap structure for the first sequence.
+ * @param gaps2 A pointer to a sequence gap structure for the second sequence.
+ * @return long The number of differences between the two sequences.
+ */
 long   computeDifferences (const char * s1,
                            const char *s2,
                            const unsigned long L,
@@ -61,22 +109,152 @@ long   computeDifferences (const char * s1,
                            const sequence_gap_structure * = NULL);
 
 
+/**
+ * @brief Get the length of a string in a vector of strings.
+ * 
+ * @param lengths A vector of string lengths.
+ * @param index The index of the string to get the length of.
+ * @return long The length of the string.
+ */
 long stringLength (Vector& lengths, unsigned long index);
+/**
+ * @brief Get the text of a string in a vector of strings.
+ * 
+ * @param strings A string buffer containing the strings.
+ * @param lengths A vector of string lengths.
+ * @param index The index of the string to get the text of.
+ * @return char* A pointer to the text of the string.
+ */
 char* stringText (const StringBuffer& strings, const Vector& lengths, unsigned long index);
+/**
+ * @brief Add a sequence to a list of sequences.
+ * 
+ * @param sequences A string buffer to add the sequence to.
+ * @param seqLengths A vector of sequence lengths.
+ * @param firstSequenceLength The length of the first sequence.
+ * @param names A string buffer of sequence names.
+ * @param nameLengths A vector of sequence name lengths.
+ */
 void addASequenceToList (StringBuffer& sequences, Vector& seqLengths, long &firstSequenceLength, StringBuffer& names, Vector& nameLengths);
+/**
+ * @brief Read a FASTA file.
+ * 
+ * @param F The file to read from.
+ * @param automatonState The state of the automaton.
+ * @param names A string buffer to store the names of the sequences in.
+ * @param sequences A string buffer to store the sequences in.
+ * @param nameLengths A vector to store the lengths of the names in.
+ * @param seqLengths A vector to store the lengths of the sequences in.
+ * @param firstSequenceLength The length of the first sequence.
+ * @param oneByOne A flag to indicate whether to read the sequences one byone.
+ * @param sequenceInstances A vector to store the sequence instances in.
+ * @param sep The separator to use between the name and the sequence.
+ * @param include_prob The probability of including a sequence.
+ * @param show_progress A flag to indicate whether to show progress.
+ * @return int The number of sequences read.
+ */
 int readFASTA (FILE* F, char& automatonState,  StringBuffer &names, StringBuffer& sequences, Vector &nameLengths, Vector &seqLengths, long& firstSequenceLength, bool oneByOne = false,  Vector* sequenceInstances = NULL, char sep = ':', double include_prob = 1.0, bool show_progress = false);
+/**
+ * @brief Dump a sequence to a FASTA file.
+ * 
+ * @param index The index of the sequence to dump.
+ * @param output The file to dump the sequence to.
+ * @param firstSequenceLength The length of the first sequence.
+ * @param d A pointer to an array of doubles.
+ * @param append A flag to indicate whether to append to the file.
+ * @param from The starting position of the sequence to dump.
+ * @param to The ending position of the sequence to dump.
+ */
 void dump_sequence_fasta (unsigned long index, FILE* output, long firstSequenceLength, double * d = NULL, bool = false, unsigned long from = 0L, unsigned long to = 0L);
+/**
+ * @brief Initialize the alphabets.
+ * 
+ * @param dna A flag to indicate whether to initialize the DNA alphabet.
+ * @param map A pointer to a character map.
+ * @param id_map A flag to indicate whether to use an identity map.
+ */
 void initAlphabets(bool = false, char * = NULL, bool id_map = false);
+/**
+ * @brief Merge two sequences.
+ * 
+ * @param source The source sequence.
+ * @param target The target sequence.
+ * @param sequence_length The length of the sequences.
+ */
 void merge_two_sequences (const char* source, char* target, const long sequence_length);
+/**
+ * @brief Check if two sequences are a perfect match.
+ * 
+ * @param source The source sequence.
+ * @param target The target sequence.
+ * @param sequence_length The length of the sequences.
+ * @return long The number of mismatches.
+ */
 long perfect_match (const char* source, char* target, const long sequence_length);
+/**
+ * @brief Dump a sequence to a FASTA file.
+ * 
+ * @param source The sequence to dump.
+ * @param sequence_length The length of the sequence.
+ * @param output The file to dump the sequence to.
+ * @param newln A flag to indicate whether to add a newline.
+ * @param append A flag to indicate whether to append to the file.
+ * @param from The starting position of the sequence to dump.
+ * @param to The ending position of the sequence to dump.
+ */
 void dump_fasta (const char* source, const long sequence_length, FILE* output, bool newln = true, bool = false, unsigned long from = 0L, unsigned long to = 0L);
 
+/**
+ * @brief Reverse complement a sequence.
+ * 
+ * @param sequence The sequence to reverse complement.
+ * @param from The starting position of the sequence to reverse complement.
+ * @param to The ending position of the sequence to reverse complement.
+ * @return int 0 on success, 1 on failure.
+ */
 int    reverseComplement (StringBuffer& sequence, unsigned long from, unsigned long to);
+/**
+ * @brief Describe the gaps in a sequence.
+ * 
+ * @param source The sequence to describe.
+ * @param sequence_length The length of the sequence.
+ * @param char_count The number of characters in the alphabet.
+ * @return sequence_gap_structure A struct containing the first and last non-gap characters in the sequence.
+ */
 struct sequence_gap_structure describe_sequence (const char* source, const unsigned long sequence_length, const unsigned long char_count = 4UL);
 
+/**
+ * @brief Resolve an ambiguous character.
+ * 
+ * @param c The character to resolve.
+ * @param dna A flag to indicate whether to resolve the character as DNA.
+ * @param protein A flag to indicate whether to resolve the character as a protein.
+ * @return const long* A pointer to an array of resolved characters.
+ */
 const long * resolve_char (unsigned char, bool = false, bool = true);
+/**
+ * @brief Get the number of resolutions for an ambiguous character.
+ * 
+ * @param c The character to get the number of resolutions for.
+ * @param dna A flag to indicate whether to get the number of resolutions for the character as DNA.
+ * @return const double The number of resolutions for the character.
+ */
 const double resolution_count (unsigned char, bool = false);
+/**
+ * @brief Unmap a character.
+ * 
+ * @param c The character to unmap.
+ * @param dna A flag to indicate whether to unmap the character as DNA.
+ * @return const char The unmapped character.
+ */
 const char unmap_char (unsigned char, bool = false);
+/**
+ * @brief Unpack a difference into a location and an alternative.
+ * 
+ * @param diff The difference to unpack.
+ * @param location The location of the difference.
+ * @param alt The alternative of the difference.
+ */
 inline void unpack_difference (long diff, long& location, unsigned& alt) {
     location = diff >> 8;
     alt = diff & 0xff;
@@ -100,3 +278,4 @@ extern const  double  resolutionsCount [];
 extern char validFlags[];
 
 #endif
+''
